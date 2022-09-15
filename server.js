39dd7d692b1b8a3ea0186776f98086ce21d2e72c
vakiko <victoria.c6140@gmail.com>
2022-09-15 13:13:20 -0500

    implemented simple addition to calculator app

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+//jshint esversion:6
+
 const express = require("express");
+const bodyparser = require("body-parser");
 
 const app = express();
+app.use(bodyparser.urlencoded({extended: true})) //needed for bodyparser
 
-app.get("/", function(req,res){
+
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html"); //const of the directory
 });
 
-app.post("/", function(req,res){
-    res.send("thanks for sending that!");
+app.post("/", function(req, res){
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result is: " + result);
+    
 });
+
+
 
 app.listen(3000,function(){
     console.log("Hello!!");
